39267aa51a899941f1abb49cf0a97cbe60575c8a
hamzeh banymelhem <85110553+hamzeh27@users.noreply.github.com>
2021-06-23 16:52:23 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,78 @@
+let name = prompt ("what is your name?") ;
+alert ('welcome'+ name);
+let age = prompt ("how old are you");
+if ( age>18 ) {
+alert ("you are welcome")
+console.log ("wlecome")
+}
+else{
+    alert ("go to home and watch cartoon ")
+console.log ("you are child")
+};
+let lastname = prompt ("what is your last name") ;
+alert (name + lastname);
+let work = prompt ("are you work ? note:anwser with yes/y or no/n " );
+switch (work.toLowerCase()) {
+case 'yes':
+case 'y':
+alert('yes, I am work  ' );
+console.log ("yes iam work ");
+break;
+case 'no':
+case 'n':
+alert('no, i dont work' );
+console.log ("no i dont work ");
+break;
+       }
+let typeofwork = prompt ("do you like your work ? note:anwser with yes/y or no/n " );
+switch (typeofwork.toLowerCase()) {
+    case 'yes':
+        case 'y':
+        alert('yes i like my work ');
+        console.log("yes i like my work ");
+        break;
+        case 'no':
+          case 'n':
+          alert('no, I dont like my work  ');
+          console.log("no i dont  like my work ");
+          break;
+       }
+let salary = prompt ("are the salary is good ?note:anwser with yes/y or no/n ");
+switch (salary.toLowerCase()) {
+    case 'yes':
+        case 'y':
+        alert('yes i like my work ');
+        console.log("yes i like my work ");
+        break;
+        case 'no':
+          case 'n':
+          alert('no, I dont like my work  ');
+          console.log("no i dont  like my work ");
+          break;
+       }
+       let future = prompt ("are you want to change your carer ? note : answer with yes/ y or no /n ");
+       switch (future.toLowerCase()) {
+        case 'yes':
+            case 'y':
+            alert('yes i want do that ');
+            console.log("yes i want do that ");
+            break;
+            case 'no':
+              case 'n':
+              alert('no, I dont  ');
+              console.log("no i dont  ");
+              break;}
+let result = prompt ("are you honset with answer previous questions  ?note : answer with yes/ y or no /n ");
+       switch (result.toLowerCase()) {
+        case 'yes':
+            case 'y':
+            alert('thanks  ');
+            console.log("yes  ");
+            break;
+            case 'no':
+              case 'n':
+              alert('do that agian and plz be honset ');
+              console.log("no i dont  ");
+              break;
+            }
+prompt ("thanks for visit "); 
